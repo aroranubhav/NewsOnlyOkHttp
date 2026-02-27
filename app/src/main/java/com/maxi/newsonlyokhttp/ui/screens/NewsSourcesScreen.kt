59ef8f115e91c976c1982e181b535340e45051df
maxi <arora.anubhav93@gmail.com>
2026-02-27 12:15:15 +0530

    Updated screen logic

--- app/src/main/java/com/maxi/newsonlyokhttp/ui/screens/NewsSourcesScreen.kt
+++ app/src/main/java/com/maxi/newsonlyokhttp/ui/screens/NewsSourcesScreen.kt
@@ -3,6 +3,7 @@ package com.maxi.newsonlyokhttp.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -31,7 +32,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -44,7 +44,6 @@ import com.maxi.newsonlyokhttp.domain.model.NewsSource
 import com.maxi.newsonlyokhttp.ui.common.NewsSourcesUiState
 import com.maxi.newsonlyokhttp.ui.common.UiEvent
 import com.maxi.newsonlyokhttp.ui.viewmodel.NewSourcesViewModel
-import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -83,7 +82,7 @@ fun NewsSourcesScreen() {
 
                     is UiEvent.RefreshStarted -> {
                         isRefreshing = true
-                        event.message
+                        null
                     }
 
                     is UiEvent.RefreshCompleted -> {
@@ -91,7 +90,7 @@ fun NewsSourcesScreen() {
                         event.message
                     }
                 }
-                scope.launch {
+                message?.let {
                     snackBarHostState.showSnackbar(
                         message,
                         duration = SnackbarDuration.Short
@@ -107,7 +106,7 @@ fun NewsSourcesScreen() {
                     Text(
                         text = "News Sources",
                         style = MaterialTheme.typography.headlineLarge,
-                        color = Color.Red,
+                        color = MaterialTheme.colorScheme.primary,
                         fontWeight = FontWeight.SemiBold,
                         fontStyle = FontStyle.Italic
                     )
@@ -123,11 +122,10 @@ fun NewsSourcesScreen() {
                 .fillMaxSize()
                 .padding(paddingValues)
         ) {
-            when (uiState) {
+            when (val state = uiState) {
                 is NewsSourcesUiState.Success -> {
-                    val sources = (uiState as NewsSourcesUiState.Success).data
                     NewsSourcesList(
-                        sources,
+                        state.data,
                         isRefreshing = isRefreshing,
                         onRefresh = {
                             viewModel.refreshSources()
@@ -170,8 +168,9 @@ fun NewsSourcesList(
             )
     ) {
         LazyColumn(
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            contentPadding = PaddingValues(8.dp)
         ) {
             items(sources) { item ->
                 NewsSourceItem(item, onClick)
@@ -182,7 +181,7 @@ fun NewsSourcesList(
             isRefreshing = isRefreshing,
             modifier = Modifier
                 .align(Alignment.TopCenter),
-            color = Color.Red
+            color = MaterialTheme.colorScheme.primary
         )
     }
 }
@@ -196,7 +195,10 @@ fun NewsSourceItem(
         elevation = CardDefaults.cardElevation(8.dp),
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 12.dp, vertical = 8.dp),
+            .padding(
+                horizontal = 12.dp,
+                vertical = 8.dp
+            ),
         onClick = {
             onClick(source.name)
         }
@@ -250,7 +252,7 @@ fun NoSourcesView() {
         Text(
             text = "No news sources found!",
             style = MaterialTheme.typography.bodySmall,
-            color = Color.Red,
+            color = MaterialTheme.colorScheme.error,
             fontWeight = FontWeight.SemiBold,
             fontStyle = FontStyle.Normal
         )
